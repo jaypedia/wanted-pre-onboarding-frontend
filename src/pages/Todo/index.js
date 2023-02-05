475574c5f940d9952f80688eafb5c282b30e2778
Millie <jay.lee.2280@gmail.com>
2023-02-05 12:27:17 +0900

    [#9] Feat: 투두 추가 기능 구현

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,15 +1,34 @@
 import { useLoaderData } from 'react-router-dom';
+import { useInput } from 'hooks/useInput';
+import { createTodo } from 'apis/todoApi';
+import { useState } from 'react';
 
 export const Todo = () => {
-  const { data: todoData } = useLoaderData();
+  const { data: loadedTodoData } = useLoaderData();
+  const [todos, setTodos] = useState(loadedTodoData);
+  const { value: todoValue, onReset: resetTodo, onChange: onTodoChange } = useInput();
+
+  const handleCreateTodo = async (e) => {
+    e.preventDefault();
+    if (!todoValue) return;
+    const newTodo = await createTodo({ todo: todoValue });
+    setTodos((prev) => [...prev, newTodo]);
+    resetTodo();
+  };
 
   return (
     <>
       <h1>✅ Todo List</h1>
-
+      <form onSubmit={handleCreateTodo}>
+        <h3>➕ 새로운 할 일 추가하기</h3>
+        <input data-testid="new-todo-input" value={todoValue} onChange={onTodoChange} />
+        <button type="submit" data-testid="new-todo-add-button">
+          추가
+        </button>
+      </form>
       <h3>📄 나의 할 일 목록</h3>
       <ul>
-        {todoData.map(({ id, todo, isCompleted }) => {
+        {todos.map(({ id, todo, isCompleted }) => {
           return (
             <li key={id}>
               <label>
