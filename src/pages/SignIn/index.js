c8d90ce664598cdf0c12b90384e7af2535517af7
Millie <jay.lee.2280@gmail.com>
2023-02-05 10:30:54 +0900

    [#4] Feat: 회원가입과 로그인 페이지에 이메일과 비밀번호의 유효성 검사 기능 구현

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,35 @@
+import { useCheckAccount } from 'hooks/useCheckAccount';
+
 export const SignIn = () => {
+  const {
+    isButtonDisabled,
+    emailRef,
+    passwordRef,
+    emailInput,
+    passwordInput,
+    handleEmailChange,
+    handlePasswordChange,
+  } = useCheckAccount();
+
   return (
     <>
-      <div>Sign In.</div>
+      <h1>로그인</h1>
       <div>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button data-testid="signup-button">로그인하기</button>
+        <input
+          data-testid="email-input"
+          ref={emailRef}
+          value={emailInput}
+          onChange={handleEmailChange}
+        />
+        <input
+          data-testid="password-input"
+          ref={passwordRef}
+          value={passwordInput}
+          onChange={handlePasswordChange}
+        />
+        <button data-testid="signin-button" disabled={isButtonDisabled}>
+          로그인하기
+        </button>
       </div>
     </>
   );
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,35 @@
+import { useCheckAccount } from 'hooks/useCheckAccount';
+
 export const SignUp = () => {
+  const {
+    isButtonDisabled,
+    emailRef,
+    passwordRef,
+    emailInput,
+    passwordInput,
+    handleEmailChange,
+    handlePasswordChange,
+  } = useCheckAccount();
+
   return (
     <>
-      <div>Sign Up</div>
+      <h1>회원가입</h1>
       <div>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button data-testid="signup-button">회원가입하기</button>
+        <input
+          data-testid="email-input"
+          ref={emailRef}
+          value={emailInput}
+          onChange={handleEmailChange}
+        />
+        <input
+          data-testid="password-input"
+          ref={passwordRef}
+          value={passwordInput}
+          onChange={handlePasswordChange}
+        />
+        <button data-testid="signup-button" disabled={isButtonDisabled}>
+          회원가입하기
+        </button>
       </div>
     </>
   );
